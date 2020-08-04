154a4495b82d306fa7bddc5eadbaf1b5b9139d36
Thomas Rasmussen <thomasrasmussen@Thomass-MacBook-Pro-4.local>
2020-08-04 13:05:37 +0200

    lang toggle commented out

--- js/index.js
+++ js/index.js
@@ -1,35 +1,82 @@
 window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 }
 
 window.onresize = () => {
-  initLineProjects();
+    initLinesProjects();
 }
 
 window.addEventListener('load', (event) => {
-  initDelay();
-  initLineProjects();
+    initDelay();
+    initLinesProjects();
+    initChangeLang();
 });
 
 function initDelay() {
-  var delayElements = document.querySelectorAll('.delay');
-  var overlayElements = document.querySelectorAll('.overlay');
-  delayElements.forEach(element => {
-    element.classList.add('show');
-  });
+    var delayElements = document.querySelectorAll('.delay');
+    var overlayElements = document.querySelectorAll('.overlay');
+    delayElements.forEach(element => {
+        element.classList.add('show');
+    });
 
-  overlayElements.forEach(element => {
-    element.classList.add('show');
-  });
+    overlayElements.forEach(element => {
+        element.classList.add('show');
+    });
 }
 
-function initLineProjects() {
-  var projectElm = document.querySelector('.js-projects');
-  var projectLineElm = document.querySelector('.js-projects-line');
-  var projectOverlayElm = document.querySelector('.js-projects-overlay');
+function resetDelay() {
+    var delayElements = document.querySelectorAll('.delay');
+    var overlayElements = document.querySelectorAll('.overlay');
 
-  setTimeout(() => {
-    var height = projectElm.getBoundingClientRect();
-    projectLineElm.style.height = `${height.height - 7}px`;
-  }, 100)
+    delayElements.forEach(element => {
+        element.classList.remove('show');
+        element.offsetHeight;
+    });
+
+    overlayElements.forEach(element => {
+        element.classList.remove('show');
+        element.offsetHeight;
+    });
+}
+
+function initLinesProjects() {
+    var projectElm = document.querySelector('.js-projects');
+    var projectLineElms = document.querySelectorAll('.js-projects-line');
+
+    setTimeout(() => {
+        projectLineElms.forEach(element => {
+            var height = projectElm.getBoundingClientRect();
+            element.style.height = `${height.height - 7}px`;
+        });
+
+    }, 100);
+}
+
+function initChangeLang() {
+    document.querySelector('.js-lang-selector').addEventListener('click', (e) => {
+        var langSelect = document.querySelector('.js-lang-selector');
+        var language = langSelect.getAttribute('data-value');
+        var langActive = langSelect.querySelector('.js-lang.active');
+        var langHidden = langSelect.querySelector('.js-lang:not(.active)');
+        var langDaContainer = document.querySelector('.js-main-container.da');
+        var langEngContainer = document.querySelector('.js-main-container.eng');
+        
+        if (language === 'da') {
+            langEngContainer.classList.remove('hide');
+            langDaContainer.classList.add('hide');
+            langSelect.setAttribute('data-value', 'eng');
+        } else {
+            langDaContainer.classList.remove('hide');
+            langEngContainer.classList.add('hide');
+            langSelect.setAttribute('data-value', 'da');
+        }
+
+        langActive.classList.remove('active');
+        langHidden.classList.add('active');
+
+        window.scrollTo(0, 0);
+        resetDelay();
+        initDelay();
+        initLinesProjects();
+    });
 }
